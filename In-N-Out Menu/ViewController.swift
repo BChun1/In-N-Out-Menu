b8db26472ac1f311387090e4e2e73b8ddca601a9
BChun1 <BChun37@yahoo.com>
2017-03-17 10:18:29 -0700

    Initial Commit

---
+++ In-N-Out Menu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  In-N-Out Menu
+//
+//  Created by Brian Chun on 3/17/17.
+//  Copyright © 2017 Brian Chun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
